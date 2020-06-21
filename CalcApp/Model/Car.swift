fce2e74304d94aec1967f4439ff6314ac51d80c7
akino-programmer <noguchi_a7@yahoo.co.jp>
2020-06-21 17:25:33 +0900

    first commit.

---
+++ CalcApp/Model/Car.swift
@@ -0,0 +1,23 @@
+//
+//  Car.swift
+//  CalcApp
+//
+//  Created by 野口晃 on 2020/06/21.
+//  Copyright © 2020 Akira Noguchi. All rights reserved.
+//
+
+import Foundation
+
+class Car{
+    
+    var frontWheel = 0
+    var rearWheel = 0
+    
+    //イニシャライズ　初期化
+    init(){
+        
+        
+    }
+    
+    
+}
