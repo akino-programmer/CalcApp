641f47cb568a8fc439cf223765f46ea4086dbdb2
akino-programmer <noguchi_a7@yahoo.co.jp>
2020-06-21 22:54:31 +0900

    car function.

--- CalcApp/Model/Car.swift
+++ CalcApp/Model/Car.swift
@@ -17,7 +17,29 @@ class Car{
     init(){
         
         
+        frontWheel = 2
+        rearWheel = 2
+        
+        
     }
+    
+    func drive(){
+        
+        print("運転開始！！")
+        print("前輪　　\(frontWheel)")
+        print("後輪　　\(rearWheel)")
+    }
+    
+    func move(toBack:String){
+        
+        print(toBack)
+        
+    }
+    
+    func plusAndMinus(num1:Int,num2:Int)->Int{
+         
+         return num1 + num2
+     }
     
     
 }
--- CalcApp/ViewController.swift
+++ CalcApp/ViewController.swift
@@ -10,13 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var carModel = Car()
+    
+    var count = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        carModel.frontWheel = 10
+        carModel.rearWheel = 10
         
-        
+        count = 10
     }
 
-
+    @IBAction func doAction(_ sender: Any) {
+        
+        
+        carModel.drive()
+        carModel.move(toBack: "後ろに行くよ")
+        var total = carModel.plusAndMinus(num1: carModel.frontWheel, num2:carModel.rearWheel)
+        print("タイヤの合計数　\(total)")
+        
+    }
+    
+ 
+    
 }
 
